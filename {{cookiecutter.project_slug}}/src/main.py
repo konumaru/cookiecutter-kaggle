cb386e577614e299f43eefa91917b140e5424ce6
konumaru <konumaru1022@gmail.com>
2023-03-15 13:13:45 +0900

    [update] powerfull utiles

--- {{cookiecutter.project_slug}}/src/main.py
+++ {{cookiecutter.project_slug}}/src/main.py
@@ -1,6 +1,14 @@
-def main() -> None:
+import hydra
+
+from config import Config
+from utils import timer
+
+
+@hydra.main(version_base=None, config_name="config")
+def main(cfg: Config) -> None:
     print("Hello Workd!")
 
 
 if __name__ == "__main__":
-    main()
+    with timer("main.py"):
+        main()
